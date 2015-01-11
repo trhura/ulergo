5753d23a7c00698f38030f6246fdb1a084c81cae
Thura Hlaing <trhura@gmail.com>
2015-01-11 18:49:02 +0630

    added prob22

---
+++ prob22.clj
@@ -0,0 +1,18 @@
+(require '[clojure.string :as string])
+
+(defn alphabetical-value [name]
+  (apply + (map #(-> % int (- 64)) name)))
+
+(defn get-score [name position]
+  (* (alphabetical-value name) position))
+
+(defn read-names-from-file [filename]
+  (map #(subs % 1 (dec (count %))) (string/split (slurp filename) #",")))
+
+(defn prob22 []
+  (apply + (->> "p022_names.txt"
+                read-names-from-file
+                sort
+                (map alphabetical-value)
+                (map-indexed vector)
+                (map (fn [[pos value]] (* (inc pos) value))))))
